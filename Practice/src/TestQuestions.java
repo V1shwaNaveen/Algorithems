7cc57422c69cfc6c55436b4db4aea8553011b8b6
Vishwa Warnakulasuriya <vishwanaveen.personal@gmail.com>
2025-04-13 22:03:03 +0530

    Finished question 01

--- Practice/src/TestQuestions.java
+++ Practice/src/TestQuestions.java
@@ -1,5 +1,27 @@
 package findMultiples;
+import java.util.Arrays;
 
 public class TestQuestions {
-    
+
+    public static void main (String[]args){
+        int[] arr = {5, 3, 8, 2, 6, 1};
+        Arrays.sort(arr);
+        int find = 4;
+        boolean found = false;
+
+        for(int num:arr){
+            if(num == find){
+                found = true;
+            }else {
+                found = false;
+            }
+        }
+        if (found) {
+            System.out.println("Number " + find + " was found in the array!");
+        } else {
+            System.out.println("Number " + find + " is missing from the array.");
+        }
+
+    }
+
 }
