b56d302a620c36da1e2fd8eac757ac9c4efc09f8
Vishwa Warnakulasuriya <vishwanaveen.personal@gmail.com>
2025-04-14 20:25:35 +0530

    binary search implimented

--- Practice/src/BinarySearch.java
+++ Practice/src/BinarySearch.java
@@ -1,5 +1,28 @@
 package findMultiples;
 
+import java.util.Arrays;
+
 public class BinarySearch {
-    
+
+    public static void main(String[]args) {
+        int find = 9;
+        boolean found = false;
+        int[] arr = {5, 3, 8, 2, 6, 1, 7, 12, 10, 15, 13, 20, 9};
+        Arrays.sort(arr);
+
+        int first = 0;
+        int last = arr.length - 1;
+
+        while (first<last){
+            int middle = (first+last)/2;
+            if(arr[middle] == find){
+                found = true;
+                break;
+            } else if (arr[middle]>find) {
+                first = middle+1;
+            } else {
+                last = middle-1;
+            }
+        }
+    }
 }
